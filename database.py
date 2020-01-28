cdccfeb291b7950c96578c70b2d2d6f441596545
Roger Pollock <RPOLLOCK@augusta.edu>
2020-01-28 00:53:02 -0500

    Made grammar and structure fixes, added partitions from JSONs. RP

--- database.py
+++ database.py
@@ -1,101 +1,124 @@
 import _sqlite3
 import json
+
 conn = _sqlite3.connect('servers.db')
 c = conn.cursor()
-# c.execute("""CREATE TABLE server(
-#     serverId text primary key,
-#     servername text,
-#     rackID text,
-#     serverTypeID text,
-#     os text,
-#     memory text,
-#     cpu text,
-#     cpucores text,
-#     gpu text,
-#     model text,
-#     foreign key (rackID) references rack(rackID),
-#     foreign key (serverTypeID) references servertype(typeID)
+# c.execute("""CREATE TABLE Server(
+#     ServerId text primary key,
+#     ServerName text,
+#     RackID text,
+#     ServerTypeID text,
+#     Os text,
+#     Memory text,
+#     Cpu text,
+#     CpuCores text,
+#     Gpu text,
+#     Model text,
+#     foreign key (RackId) references Rack(RackId),
+#     foreign key (ServerTypeId) references ServerType(TypeId)
 #     )""")
-# c.execute("""CREATE TABLE rack(
-#     rackID text primary key,
-#     name text,
-#     locationID text,
-#     foreign key(locationID) references location(locationID)
-# )""")
-# c.execute("""CREATE TABLE location(
-#     locationID text primary key,
-#     buildingNumber text,
-#     room text
-# )""")
-# c.execute("""CREATE TABLE servertype(
-#     typeID text primary key,
-#     typeName text
+# c.execute("""CREATE TABLE Rack(
+#     RackId text primary key,
+#     Name text,
+#     LocationId text,
+#     foreign key(LocationId) references Location(LocationId)
 #     )""")
-# c.execute("""CREATE TABLE metric(
-#     metricID text primary key,
+# c.execute("""CREATE TABLE Location(
+#     LocationId text primary key,
+#     BuildingNumber text,
+#     Room text
+#     )""")
+# c.execute("""CREATE TABLE ServerType(
+#     TypeId text primary key,
+#     TypeName text
+#     )""")
+# c.execute("""CREATE TABLE Metric(
+#     MetricId text primary key,
 #     Time text,
-#     cpu real,
-#     ram real,
-#     disk real,
-#     gpu real,
-#     pingLatency real,
-#     serverId text,
-#     foreign key (serverId) references server(serverId)
+#     Cpu real,
+#     Ram real,
+#     PartA real,
+#     PartB real,
+#     PartC real,
+#     PartD real,
+#     Disk real,
+#     Gpu real,
+#     PingLatency real,
+#     ServerId text,
+#     foreign key (ServerId) references Server(ServerId)
 #     )""")
-# c.execute("""CREATE TABLE service(
-#     serviceID text primary key,
-#     serviceName text,
-#     status text,
-#     running text,
-#     serverId text,
-#     foreign key (serverId) references server(serverId)
+# c.execute("""CREATE TABLE Service(
+#     ServiceId text primary key,
+#     ServiceName text,
+#     Status text,
+#     ServerId text,
+#     foreign key (ServerId) references Server(ServerId)
 #     )""")
-# c.execute("""CREATE TABLE database(
-#     databaseID text primary key,
-#     databaseName text,
-#     status text,
-#     serverId text,
-#     foreign key (serverId) references server(serverId)
+# c.execute("""CREATE TABLE Database(
+#     DatabaseId text primary key,
+#     DatabaseName text,
+#     Status text,
+#     ServerId text,
+#     foreign key (ServerId) references Server(ServerId)
 #     )""")
-# c.execute("""CREATE TABLE runningjob(
-#     coresAllocated text,
-#     reservedTime text,
-#     serverID text,
-#     user text,
-#     jobName text,
-#     startTime text,
-#     foreign key (serverID) references server(serverID)
-#     constraint runningjob_pk primary key(serverID, user, jobName, startTime)
+# c.execute("""CREATE TABLE RunningJob(
+#     CoresAllocated text,
+#     ReservedTime text,
+#     ServerId text,
+#     User text,
+#     JobName text,
+#     StartTime text,
+#     foreign key (ServerId) references Server(ServerId)
+#     constraint RunningJob_pk primary key(ServerId, User, JobName, StartTime)
 #     )""")
-with open("C:\\Users\\hicks\\source\\repos\\JsonMockDataCreator\\Jsons\\Db-01.json") as data:
-    input=json.load(data)
+with open("C:\\Users\\Roger\\source\\repos\\JsonMockDataCreator\\Jsons\\Db-01.json") as data:
+    input = json.load(data)
 
-tempserverId= input['ServerId']
-tempserverName= input['ServerName']
-tempGpu= input['Gpu']
-tempMemory= input['Memory']
-tempOs= input['Os']
-tempCpuCores= input['CpuCores']
-tempCpu= input['Cpu']
-tempModel= input['Model']
-c.execute("""insert into server(serverId,servername, gpu,memory,os,cpucores,cpu,model,serverTypeID,rackID) values(?,?,?,?,?,?,?,?,?,?)""",(tempserverId,tempserverName,tempGpu,tempMemory,tempOs,tempCpuCores,tempCpu,tempModel,input['ServerType']['TypeID'],input['Rack']['RackID']))
-c.execute("""insert into rack(rackID,name,locationID) values(?,?,?)""",(input['Rack']['RackID'],input['Rack']['Name'],input['Rack']['Location']['LocationID']))
-c.execute("""insert into location(locationID,buildingNumber,room) values (?,?,?)""", (input['Rack']['Location']['LocationID'],input['Rack']['Location']['BuildingNumber'],input['Rack']['Location']['Room']))
-c.execute("""insert into metric(metricID,Time,cpu,gpu,disk,ram,pingLatency,serverId) values(?,?,?,?,?,?,?,?)""",(input['Metric']['MetricId'],input['Metric']['Time'],input['Metric']['Cpu'],input['Metric']['Gpu'],input['Metric']['Disk'],input['Metric']['Ram'],input['Metric']['PingLatency'],tempserverId))
-c.execute("""insert into servertype(typeName,typeID) values(?,?)""",(input['ServerType']['TypeName'],input['ServerType']['TypeID']))
-
-for runningjob in input['RunningJobs']:
-    c.execute("""insert into runningjob(user,jobName,startTime,coresAllocatedreservedTime,serverID) values(?,?,?,?,?,?)""",(runningjob['RunningJobs']['User']),runningjob['RunningJobs']['JobName'],runningjob['RunningJobs']['StartTime'],runningjob['RunningJobs']['CoresAllocated'],runningjob['RunningJobs']['ReservedTime'],tempserverId)
-for database in input['Databases']:
-    tempddbId= database['DatabaseId']
-    tempInput=database['DatabaseName']
-    tempStatus=database['Status']
-    c.execute("""insert into database(databaseID, databaseName,status,serverId) values (?,?,?,?)""",(tempddbId,tempInput,tempStatus,tempserverId))
-for service in input['Services']:
-    tempserviceID= service['ServiceID']
-    tempServiceName= service['ServiceName']
-    tempserviceStatus= service['Status']
-    c.execute("""insert into service(serviceID, serviceName,status,serverId) values (?,?,?,?)""", (tempserviceID,tempServiceName,tempserviceStatus,tempserverId))
+tempServerId = input['ServerId']
+tempServerName = input['ServerName']
+tempGpu = input['Gpu']
+tempMemory = input['Memory']
+tempOs = input['Os']
+tempCpuCores = input['CpuCores']
+tempCpu = input['Cpu']
+tempModel = input['Model']
+c.execute("""insert into Server(ServerId, ServerName, Gpu, Memory, Os, CpuCores, Cpu, Model, ServerTypeId,
+RackId) values(?,?,?,?,?,?,?,?,?,?)""", (tempServerId, tempServerName, tempGpu, tempMemory, tempOs,
+                                         tempCpuCores,
+                                         tempCpu, tempModel, input['ServerType']['TypeId'],
+                                         input['Rack']['RackId']))
+c.execute("""insert into Rack(RackId, Name, LocationId) values(?,?,?)""", (input['Rack']['RackId'],
+                                                                           input['Rack']['Name'],
+                                                                           input['Rack']['Location']['LocationId']))
+c.execute("""insert into Location(LocationId, BuildingNumber, Room) values (?,?,?)""", (
+    input['Rack']['Location']['LocationId'], input['Rack']['Location']['BuildingNumber'],
+    input['Rack']['Location']['Room']))
+c.execute("""insert into Metric(MetricId, Time, Cpu, Gpu, PartA, PartB, PartC, PartD, Disk, Ram, PingLatency,
+ServerId) values(?,?,?,?,?,?,?,?,?,?,?,?)""", (input['Metric']['MetricId'], input['Metric']['Time'],
+                                               input['Metric']['Cpu'], input['Metric']['Gpu'], input['Metric']['PartA'],
+                                               input['Metric']['PartB'], input['Metric']['PartC'],
+                                               input['Metric']['PartD'], input['Metric']['Disk'],input['Metric']['Ram'],
+                                               input['Metric']['PingLatency'], tempServerId))
+c.execute("""insert into ServerType(TypeName, TypeId) values(?,?)""",
+          (input['ServerType']['TypeName'], input['ServerType']['TypeId']))
+for RunningJob in input['RunningJobs']:
+    c.execute(
+        """insert into RunningJob(User, JobName, StartTime, CoresAllocatedReservedTime, serverID) values(?,?,?,?,?,?)""",
+        (RunningJob['RunningJobs']['User']), RunningJob['RunningJobs']['JobName'],
+        RunningJob['RunningJobs']['StartTime'], RunningJob['RunningJobs']['CoresAllocated'],
+        RunningJob['RunningJobs']['ReservedTime'], tempServerId)
+for Database in input['Databases']:
+    tempDbId = Database['DatabaseId']
+    tempInput = Database['DatabaseName']
+    tempStatus = Database['Status']
+    c.execute("""insert into Database(DatabaseId, DatabaseName, Status, ServerId) values (?,?,?,?)""",
+              (tempDbId, tempInput, tempStatus, tempServerId))
+for Service in input['Services']:
+    tempServiceId = Service['ServiceId']
+    tempServiceName = Service['ServiceName']
+    tempServiceStatus = Service['Status']
+    c.execute("""insert into Service(ServiceId, ServiceName, Status, ServerId) values (?,?,?,?)""",
+              (tempServiceId, tempServiceName, tempServiceStatus, tempServerId))
 
 conn.commit()
 conn.close()
