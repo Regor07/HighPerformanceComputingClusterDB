d15d47847fb3d502ce53f2f00821e8b35bff3274
hicks <jeshicks@augusta.edu>
2020-02-10 15:39:41 -0500

    filter working, still need to be able to move the files to archive or spam files are now moving appropriately flushes are working for running job, database, and services

--- testspace.py
+++ testspace.py
@@ -49,22 +49,52 @@ for item in samplelist:
     tempCpu = input['Cpu']
     tempModel = input['Model']
     tempRackId= input['Rack']['RackId']
-    c.execute("""update Server
-    set
-    Gpu= ?,
-    Memory= ?,
-    Os=?,
-    CpuCores=?,
-    Cpu= ?,
-    Model= ?,
-    RackId= ?
-    where serverId = '%s'""" %tempServerId,(tempGpu,tempMemory,tempOs,tempCpuCores,tempCpu,tempModel,tempRackId)
-    )
     # c.execute("""update Server
     # set
     # Gpu='test'
     # where Gpu='Nvidia 1'""")
     if isOnMasterList==True:
+        c.execute("""update Server
+        set
+        Gpu= ?,
+        Memory= ?,
+        Os=?,
+        CpuCores=?,
+        Cpu= ?,
+        Model= ?,
+        RackId= ?
+        where serverId = '%s'""" % tempServerId, (tempGpu, tempMemory, tempOs, tempCpuCores, tempCpu, tempModel, tempRackId))
+
+        # c.execute("""insert into Metric(MetricId, Time, Cpu, Gpu, PartA, PartB, PartC, PartD, Disk, Ram, PingLatency,
+        #     ServerId) values(?,?,?,?,?,?,?,?,?,?,?,?)""", (input['Metric']['MetricId'], input['Metric']['Time'],
+        #                                                    input['Metric']['Cpu'], input['Metric']['Gpu'],
+        #                                                    input['Metric']['PartA'],
+        #                                                    input['Metric']['PartB'], input['Metric']['PartC'],
+        #                                                    input['Metric']['PartD'], input['Metric']['Disk'],
+        #                                                    input['Metric']['Ram'],
+        #                                                    input['Metric']['PingLatency'], tempServerId))
+        c.execute("""delete from Database where serverId='%s'""" % tempServerId)
+        for Database in input['Databases']:
+            tempDbId = Database['DatabaseId']
+            tempInput = Database['DatabaseName']
+            tempStatus = Database['Status']
+            c.execute("""insert into Database(DatabaseId, DatabaseName, Status, ServerId) values (?,?,?,?)""",
+                      (tempDbId, tempInput, tempStatus, tempServerId))
+        c.execute("""delete from Service where serverId='%s'""" % tempServerId)
+        for Service in input['Services']:
+            tempServiceId = Service['ServiceId']
+            tempServiceName = Service['ServiceName']
+            tempServiceStatus = Service['Status']
+            c.execute("""insert into Service(ServiceId, ServiceName, Status, ServerId) values (?,?,?,?)""",
+                      (tempServiceId, tempServiceName, tempServiceStatus, tempServerId))
+        c.execute("""delete from RunningJob where serverId='%s'"""%tempServerId)
+        for RunningJob in input['RunningJobs']:
+            c.execute(
+                """insert into RunningJob(User, JobName, StartTime, CoresAllocatedReservedTime, serverID) values(?,?,?,?,?,?)""",
+                (RunningJob['RunningJobs']['User']), RunningJob['RunningJobs']['JobName'],
+                RunningJob['RunningJobs']['StartTime'], RunningJob['RunningJobs']['CoresAllocated'],
+                RunningJob['RunningJobs']['ReservedTime'], tempServerId)
+
         print(tempServerId+' was moved to the archive folder!')
         shutil.move("C:\\Users\\hicks\\source\\repos\\JsonMockDataCreator\\Jsons\\"+dingle,"C:\\Users\\hicks\\source\\repos\\JsonMockDataCreator\\Archive")
     else:
